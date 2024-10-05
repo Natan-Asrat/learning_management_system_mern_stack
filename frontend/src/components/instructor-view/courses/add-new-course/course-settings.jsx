d208a224c5bfaa58e8d164f962438febcd6b512c
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-05 13:10:08 +0300

    course image setting

--- frontend/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ frontend/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import {Label} from "@/components/ui/label"
+import {Input} from "@/components/ui/input" 
 
 const CourseSettings = () => {
   return (
-    <div>CourseSettings</div>
+   <Card>
+    <CardHeader>
+      <CardTitle>
+        Course Settings
+      </CardTitle>
+    </CardHeader>
+    <CardContent>
+      <div className="flex flex-col gap-3">
+        <Label>
+          Upload Course Image
+        </Label>
+        <Input
+        type="file"
+        accept="image/*"
+        >
+        </Input>
+      </div>
+    </CardContent>
+   </Card>
   )
 }
 
